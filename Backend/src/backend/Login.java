a9facda338a952b45a2dcc7ae74f1f1ca1dea7f0
Tai <tainguye@usc.edu>
2021-11-16 14:02:26 -0800

    hash function

--- Backend/src/backend/Login.java
+++ Backend/src/backend/Login.java
@@ -1,9 +1,13 @@
-package backend;
+package tainguye_CSCI201_Lab1;
 
+import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 
 
-public class Login {
+public class HelloWorld {
 	public static void main(String args[]) throws Exception {
 		// post("login", "user2", "123456");
 		onLogin("login", "user2", "12345");
@@ -26,17 +30,35 @@ public class Login {
 		
 		return null;
 	}
-	
+	public static String Hash(String pwd) {
+		MessageDigest md;
+		StringBuilder sb = new StringBuilder();
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+			byte[] encodedhash = md.digest(pwd.getBytes(StandardCharsets.UTF_8));
+			for (byte b : encodedhash) {
+				sb.append(String.format("%02x", b));
+			}
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//bytes to hex
+		return sb.toString();
+		
+	}
 	public static void post(String table, String un, String pwd) {
 		String sql = "INSERT INTO " + table + " (username, password) VALUES (?, ?)";
 		try (Connection conn = getConnection();
 				PreparedStatement ps = conn.prepareStatement(sql);){
 			ps.setString(1, un);
-			ps.setString(2, pwd);
+			ps.setString(2, Hash(pwd));
+			
 			int row = ps.executeUpdate();
 		} catch (SQLException ex) {
 			System.out.println("SQLException: " + ex.getMessage());
-		}
+		} 
 	}
 	
 	public static boolean onLogin(String table, String un, String pwd) {
